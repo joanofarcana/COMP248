97a7b91c6790902291b8a7b03107a9113dac3291
Nina Prentiss <nprentiss@gmail.com>
2015-05-26 07:20:18 -0400

    added question1
    from spiderman on home laptop

---
+++ question1/QuadrilateralClassification.java
@@ -0,0 +1,19 @@
+// -----------------------------------------------------
+// COMP 248 - Assignment #2, question 1
+// Written by: Nina Prentiss, 26270611		May 20, 2015
+// General Description: 
+// ----------------------------------------------------- 
+
+package question1;
+
+import java.util.Scanner;
+
+public class QuadrilateralClassification 
+{
+	public static void main(String[] args)
+	{
+		//Declarations
+		
+		                                                                                                                           
+	}
+}
